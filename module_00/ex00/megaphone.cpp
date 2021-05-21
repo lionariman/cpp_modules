2ecf674b6127e3afe2c6acc2a7a2afad8ed33379
lionariman <lionariman@gmail.com>
2021-05-21 03:31:52 +0300

    made ex00 code shorter and easier to read

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -6,44 +6,24 @@
 /*   By: keuclide <keuclide@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 09:23:14 by keuclide          #+#    #+#             */
-/*   Updated: 2021/05/20 09:23:27 by keuclide         ###   ########.fr       */
+/*   Updated: 2021/05/21 03:29:48 by keuclide         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-void	do_uppercase(char *some_string);
-int		is_letter(char some_char);
-
 int	main(int ac, const char **av)
 {
-	if (ac == 1)
-	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-		return (0);
-	}
-	for (int i = 1; i < ac; i++)
-	{
-		do_uppercase((char *)av[i]);
-		std::cout << av[i];
-		if (i + 1 == ac)
-			std::cout << "\n";
-	}
-	return (0);
-}
+	if (ac < 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 
-int		is_letter(char some_char)
-{
-	if (some_char > 96 && some_char < 123)
-		return (1);
-	return (0);
-}
-
-void	do_uppercase(char *some_string)
-{
-	for (int i = 0; some_string[i] != '\0'; i++)
+	for (int i(1); i < ac; i++)
 	{
-		if (is_letter(some_string[i]))
-			some_string[i] = some_string[i] - 32;
+		for (int j(0); j < (int)strlen(av[i]); j++)
+			std::cout << (char)std::toupper(av[i][j]);
+		if (i < ac - 1)
+			std::cout << " ";
 	}
+	std::cout << std::endl;
+	return (0);
 }
