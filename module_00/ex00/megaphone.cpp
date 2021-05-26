b63504fa6953ab921364b08da46525399edcb2d6
lionariman <lionariman@gmail.com>
2021-05-26 16:11:33 +0300

    fix an error

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -2,9 +2,8 @@
 
 int	main(int ac, const char **av)
 {
-	if (ac < 1)
+	if (ac == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-
 	for (int i(1); i < ac; i++)
 	{
 		for (int j(0); j < (int)strlen(av[i]); j++)
